84f51a74c7aac8407accbe60d94ce3e2a0c428d7
nitinnavik <nitin.navik@tarkashilpa.com>
2023-11-09 15:04:34 +0530

    changes.

--- Components/JsonEditor.js
+++ Components/JsonEditor.js
@@ -27,7 +27,6 @@ const JsonEditor = ({
       onChange={onChange}
       waitAfterKeyPress={waitAfterKeyPress}
       onKeyPressUpdate={onKeyPressUpdate}
-      confirmGood={false}
     />
   );
 };
