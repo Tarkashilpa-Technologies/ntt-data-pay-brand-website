9884afa08812f26fcd3d4b8cbeacd878a7018fbb
Sumit Chakole <sumit.chakole@tarkashilpa.com>
2024-01-04 12:19:37 +0530

    UAT base URL configured

--- Messages/Endpoints.js
+++ Messages/Endpoints.js
@@ -2,7 +2,8 @@
 // const { API_URL } = require("../../config/env")
 // Or just specify it directly like this:
 
-export const HOST = "http://65.0.181.24:1337";
+// export const HOST = "http://65.0.181.24:1337";
+export const HOST = "https://cms-uat.nttdatapay.com";
 
 export const API_URL = `${HOST}/api`;
 export const UAT_ATOM_TECH_URL = `https://caller.atomtech.in`;
